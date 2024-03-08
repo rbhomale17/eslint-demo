b0753e7c1ae89b47abb156a122100d762ed96a88
rbhomale17 <rushikeshbhomale@gmail.com>
2024-03-08 17:52:41 +0530

    dd-8

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ console.log(str.length, str)
 const str2 = "hello"
 console.log(str2.length, str2)
 
-function str3 (str) {
-   console.log(str.length, str)
+function str3(str) {
+    console.log(str.length, str)
 }
 
 str3("jkahdkah")
+
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("hello")
+})
+
+app.listen(3000, () => {
+    console.log("listening on port 3000")
+})
