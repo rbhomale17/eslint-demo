5eb335350169a942c4ba3d89ad76362705d42294
rbhomale17 <rushikeshbhomale@gmail.com>
2024-03-08 17:33:19 +0530

    dd

---
+++ index.js
@@ -0,0 +1,11 @@
+const str = "hello"
+console.log(str.length, str)
+
+const str2 = "hello"
+console.log(str2.length, str2)
+
+function str3 (str) {
+    console.log(str.length, str)
+}
+
+str3("jkahdkah")
